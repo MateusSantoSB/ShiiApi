db0af0515eb9bd66b2ffdc933f6d295804099707
Tatetinho <141674380+Tatetinho@users.noreply.github.com>
2025-01-14 20:10:03 -0300

    edit test

--- src/test/java/com/ShiiSecret/ShiiTheSecretApplicationTests.java
+++ src/test/java/com/ShiiSecret/ShiiTheSecretApplicationTests.java
@@ -1,12 +1,11 @@
 package com.ShiiSecret;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShiiTheSecretApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
